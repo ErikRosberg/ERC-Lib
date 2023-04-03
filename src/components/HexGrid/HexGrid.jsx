63e44394efc8c8c1841f044e83950a57d72e05d7
ErikRosberg00 <Erik.Rosberg1@gmail.com>
2023-04-03 11:44:30 +0200

    changed var names and some styling to add gradient to color

--- src/components/HexGrid/HexGrid.jsx
+++ src/components/HexGrid/HexGrid.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HexContainer, Hex, HexagonContent } from "./HexGrid.style";
+import { HexContainer, HexBorder, HexagonContent } from "./HexGrid.style";
 
 function HexGrid(className, Label) {
   return (
@@ -13,18 +13,24 @@ function HexGrid(className, Label) {
       }}
     >
       <HexContainer>
-        <Hex left={2} top={1} color="red">
+        <HexBorder
+          left={2}
+          top={1}
+          direction="right"
+          firstColor="white"
+          secondColor="red"
+        >
           <HexagonContent> Hexagon 1</HexagonContent>
-        </Hex>
-        <Hex left={1} top={2} color="green">
+        </HexBorder>
+        <HexBorder left={1} top={2} color="green">
           <HexagonContent> Hexagon 2</HexagonContent>
-        </Hex>
-        <Hex left={3} top={2} color="blue">
+        </HexBorder>
+        <HexBorder left={3} top={2} color="blue">
           <HexagonContent> Hexagon 3</HexagonContent>
-        </Hex>
-        <Hex left={2} top={3} color="yellow">
+        </HexBorder>
+        <HexBorder left={2} top={3} color="yellow">
           <HexagonContent> Hexagon 4</HexagonContent>
-        </Hex>
+        </HexBorder>
       </HexContainer>
     </div>
   );
--- src/components/HexGrid/HexGrid.style.js
+++ src/components/HexGrid/HexGrid.style.js
@@ -13,7 +13,6 @@ export const HexContainer = styled.div`
   grid-template-rows: repeat(3, 1fr);
   grid-row-gap: 0;
   grid-column-gap: 0;
-  /* gap: 10px; */
   width: 100%;
   height: 100%;
   border: 1px solid red;
@@ -23,27 +22,30 @@ export const HexContainer = styled.div`
   }
 `;
 
-export const Hex = styled.span`
+export const HexBorder = styled.span`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: ${HexSizing.hexBackgroundWidth};
+  height: ${HexSizing.hexBackgroundHeight};
   grid-column-start: ${(props) => props.left};
   grid-row-start: ${(props) => props.top};
-  background-color: ${(props) => props.color};
+  background-color: ${(props) => `linear-gradient(${props.direction}, ${props.firstColor}, ${props.secondColor})`};
+  clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+`;
+
+export const HexContent = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => (props.bg ? props.bg : "white")};
   width: ${HexSizing.hexWidth};
   height: ${HexSizing.hexHeight};
   clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+
+  > p {
+    color: white;
+  }
 `;
 
 const Hex2 = styled(Hex)``;
-
-export const HexBorder = styled.span`
-  width: ${HexSizing.hexBackgroundWidth};
-  height: ${HexSizing.hexBackgroundHeight};
-  background-color: ${(props) => props.color};
-  clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
-`;
-
-export const HexagonContent = styled.span`
-  color: white;
-`;

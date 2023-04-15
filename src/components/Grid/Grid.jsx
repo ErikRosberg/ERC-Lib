7efd4909fd9244c792a55b9ee17554d692f80b63
Erikko <erik.rosberg1@gmail.com>
2023-04-15 15:45:37 +0200

    Innan test

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,13 +1,62 @@
 import React from "react";
-import { GridLayout } from "./Grid.style";
+import styled from "styled-components";
+import Hexagon from "../presentation/Hexagon/Hexagon";
+// import { GridLayout } from "./Grid.style";
+
+const GridLayout = styled.div`
+  display: grid;
+  grid-template-areas: "top top top" "left mid right" "bottom bottom bottom";
+  grid-template-columns: repeat (200px);
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 50%;
+  background-color: blue;
+
+  > div {
+  }
+
+  .top {
+    height: 100%;
+    width: 100%;
+    border: 1px solid red;
+    grid-area: top;
+  }
+  .left {
+    height: 100%;
+    width: 20rem;
+    border: 1px solid red;
+    grid-area: left;
+  }
+  .right {
+    height: 100%;
+    width: 20rem;
+    border: 1px solid red;
+    grid-area: right;
+  }
+  .bottom {
+    height: 100%;
+    width: 100%;
+    border: 1px solid red;
+    grid-area: bottom;
+  }
+`;
 
 function Grid() {
   return (
     <GridLayout>
-      <div className="top"> 1 </div>
-      <div className="left"> 2 </div>
-      <div className="right"> 3 </div>
-      <div className="bottom"> 4 </div>
+      <div className="top">
+        <Hexagon />
+      </div>
+      <div className="left">
+        <Hexagon />
+      </div>
+      <div className="right">
+        <Hexagon />
+      </div>
+      <div className="bottom">
+        <Hexagon />
+      </div>
     </GridLayout>
   );
 }
